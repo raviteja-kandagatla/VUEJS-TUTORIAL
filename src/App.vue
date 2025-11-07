fe89bf0077451911665dd4d9d052949b595776c2
Raviteja Kandagatla <ravitejakandagatla1998@gmail.com>
2025-11-07 23:10:53 +0530

    added new changes to files

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="container">
     <h1>My cool Vue App</h1>
     <button>Add Task</button>
   </div>
@@ -16,5 +16,12 @@ export default {
   text-align: center;
   color: yellowgreen;
   margin-top: 60px;
+}
+
+.container {
+  border: 1px solid steelblue;
+  padding: 30px;
+  max-width: 500px;
+  margin: auto;
 }
 </style>
