7990dd4ba5a65a7433b32241acf30baa5f7cc57e
Raviteja Kandagatla <ravitejakandagatla1998@gmail.com>
2025-11-07 21:39:44 +0530

    added new files changes

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>My cool Vue App</h1>
+    <button>Add Task</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+};
+</script>
+
+<style>
+#app {
+  text-align: center;
+  color: yellowgreen;
+  margin-top: 60px;
+}
+</style>
